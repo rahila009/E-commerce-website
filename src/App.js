3becd48be772c9c6b3925667e2a04ec8983d8d86
rahila rahim <rahilarahim009@gmail.com>
2023-05-12 11:50:12 +0500

    registration is complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 
 
 import Accounts from "./pages/account"
-
+import LoginPage from "./pages/loginPage"
 export default function App() {
     
     return(
         <>
-    <Accounts/>
+    {/* <Accounts/> */}
+    <LoginPage/>
         </>
     )
 
---
+++ src/pages/loginPage/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Login from '../../components/login'
+
+const LoginPage = () => {
+  return (
+    <div className='flex justify-center'>
+      <Login />
+    </div>
+  )
+}
+
+export default LoginPage 
+
