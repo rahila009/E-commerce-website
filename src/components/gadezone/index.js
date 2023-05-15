3f5af103fc20dededc968e686af0689e80399d6c
rahila rahim <rahilarahim009@gmail.com>
2023-05-15 11:00:59 +0500

    zone section complete

--- src/components/gadezone/index.js
+++ src/components/gadezone/index.js
@@ -1,29 +1,41 @@
 import React from "react";
 import bg1 from "../img/menimg.jpeg";
+import mobile1 from "../img/mobile.jpeg";
 
 const Zone = () => {
   return (
-    <div className="mt-[100px] px-[40px]">
+    <div className=" px-[40px]  mt-[100px] lg:grid grid-cols-2  md:grid grid-cols-2 gap-[20px]">
       {/* frist div start */}
-      <div className=" grid grid-cols-2 bg-[#DEE4D2] px-20 w-[50%] gap-5 py-[20px] ">
-        <div className="">
+      <div className=" grid grid-cols-2 bg-[#DEE4D2] px-[40px] gap-8 py-[20px]  border rounded-[12px]">
+        <div className="px-3">
           <img src={bg1} />
         </div>
         <div className="pt-6">
-          <button class="border border-gray-500 py-2 px-4  text-black transition-colors hover:border-red-500 hover:bg-red-500 hover:text-white">
+          <button class="border border-gray-200 py-2 px-4 bg-[white] text-black transition-colors hover:border-red-500 hover:bg-red-500 hover:text-white">
             GADGET ZONE
           </button>
-          <h2 className="pt-3">TAKE YOUR TRIP TO YOUR VIRTUAL WORLD</h2>
+          <h2 className="pt-7 text-[#000000]">
+            TAKE YOUR TRIP TO YOUR VIRTUAL WORLD
+          </h2>
         </div>
       </div>
       {/* frist dev end */}
-      
-      
+
       {/* second div start */}
 
-        <div>
-        
+      <div className=" bg-[#B9DCF1] grid grid-cols-2 py-6 border rounded-[12px] gap-8 ">
+        <div className=" px-3">
+          <img src={mobile1} />
         </div>
+        <div>
+          <div className="">
+            <button class=" border border-gray-200 py-2 px-4 bg-[white] text-black transition-colors hover:border-red-500 hover:bg-red-500 hover:text-white mt-5 ">
+              NEW ARRIVAL
+            </button>
+          </div>
+          <h1 className=" text-[#000000] pt-5 text-centerq">REDMI NOTE 10S</h1>
+        </div>
+      </div>
 
       {/* second div end */}
     </div>
