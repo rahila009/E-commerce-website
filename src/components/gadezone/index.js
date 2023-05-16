815e34d24fad81b45616143c779d5bdbb7d28016
rahila rahim <rahilarahim009@gmail.com>
2023-05-16 10:45:42 +0500

    zone section is complet

--- src/components/gadezone/index.js
+++ src/components/gadezone/index.js
@@ -1,39 +1,51 @@
 import React from "react";
-import bg1 from "../img/menimg.jpeg";
-import mobile1 from "../img/mobile.jpeg";
+import bg1 from "../img/menimg.png";
+import mobile1 from "../img/mobile.png";
 
 const Zone = () => {
   return (
-    <div className=" px-[40px]  mt-[100px] lg:grid grid-cols-2  md:grid grid-cols-2 gap-[20px]">
+    <div className=" px-[20px]  mt-[100px] lg:grid-cols-2  md:grid md:grid-cols-1  md:gap-8">
       {/* frist div start */}
-      <div className=" grid grid-cols-2 bg-[#DEE4D2] px-[40px] gap-8 py-[20px]  border rounded-[12px]">
+      <div className=" grid grid-cols-2 bg-[#DEE4D2] gap-8 py-[20px]  border rounded-[12px]">
         <div className="px-3">
           <img src={bg1} />
         </div>
         <div className="pt-6">
-          <button class="border border-gray-200 py-2 px-4 bg-[white] text-black transition-colors hover:border-red-500 hover:bg-red-500 hover:text-white">
+          <button class=" border border-gray-200 py-2 lg:px-5 md:px-4 px-3 bg-[white] text-black transition-colors hover:border-red-500 hover:bg-red-500 hover:text-white text-[20px] leading-[23px] uppercase  ">
             GADGET ZONE
           </button>
-          <h2 className="pt-7 text-[#000000]">
+          <h2 className="pt-7 text-[#000000] text-[20px] leading-[23px] uppercase  ">
             TAKE YOUR TRIP TO YOUR VIRTUAL WORLD
           </h2>
+          <p className="pt-2">
+            <a href="#" class="underline hover:no-underline pt-1">
+              Shop now
+            </a>
+          </p>
         </div>
       </div>
       {/* frist dev end */}
 
       {/* second div start */}
 
-      <div className=" bg-[#B9DCF1] grid grid-cols-2 py-6 border rounded-[12px] gap-8 ">
+      <div className=" bg-[#B9DCF1] grid grid-cols-2  mt-[20px] lg:mt-0  py-6 border  rounded-[12px] lg:gap-[20px] md:gap-[10px]  ">
         <div className=" px-3">
           <img src={mobile1} />
         </div>
         <div>
           <div className="">
-            <button class=" border border-gray-200 py-2 px-4 bg-[white] text-black transition-colors hover:border-red-500 hover:bg-red-500 hover:text-white mt-5 ">
+            <button class="lg:px-5 md:px-4 px-3     border border-gray-200 py-2  bg-[white] text-black transition-colors hover:border-red-500 hover:bg-red-500 hover:text-white mt-5 text-[20px] leading-[23px] uppercase ">
               NEW ARRIVAL
             </button>
           </div>
-          <h1 className=" text-[#000000] pt-5 text-centerq">REDMI NOTE 10S</h1>
+          <h1 className="  pt-5 text-centerq text-[20px] leading-[23px] uppercase ">
+            REDMI <br /> NOTE 10S
+          </h1>
+          <p className="pt-2">
+            <a href="#" class="underline hover:no-underline ">
+              Shop now
+            </a>
+          </p>
         </div>
       </div>
 
