40e156f5ce50dc82c5ff3348469971e717bb1002
hadi6788 git config --global user.name hadi6788 <hadi.furc@gmail.com>
2023-05-16 10:55:22 +0500

    resolve conflicts and merge registration branch in dev

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React from "react";
 import Slider from "./components/Slider";
 import Zone from "./components/gadegetzone";
-import Accounts from "./pages/account"
-import LoginPage from "./pages/loginPage"
+import Accounts from "./pages/account";
+import LoginPage from "./pages/loginPage";
 export default function App() {
-    
-    return(
-        <>
- <Zone/>
-<Slider/>
-    <Accounts/>
-    {/* <LoginPage/> */}
-        </>
-    )
-
-   
+  return (
+    <>
+      <Slider />
+      <Accounts />
+      <LoginPage/>
+    </>
+  );
 }
