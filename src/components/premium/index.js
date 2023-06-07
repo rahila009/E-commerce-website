4cca4cadae69c871e944cbf0425d1fa671e56766
rahila rahim <rahilarahim009@gmail.com>
2023-06-07 13:10:32 +0500

    edit contant

---
+++ src/components/premium/index.js
@@ -0,0 +1,91 @@
+import React from "react";
+
+
+const Premium = () => {
+  return (
+    <>
+      <div className="container mx-auto mt-[100px] px-[40px]  bg-gray-200 mb-8 py-10">
+        <div className="grid grid-cols-2">
+          <div>
+            <h1 className="text-[36px] leading-[40px] text-[#000000] font-bold px-8">The world’s premium brands in one destination. </h1>
+          </div>
+          <div>
+            <p className=" text-gray-500 px-8">
+              Duis aute irure dolor in reprehenderit in voluptate velit esse
+              cillum eu fugiat nulla pariatur.
+            </p>
+          </div>
+        </div>
+
+       
+        <div className=" mx-auto  flex justify-center gap-2 mt-10 px[20px]">
+     
+     
+        <div className="bg-white py-10 rounded-[10px] mx-4 px-6">
+          <img
+            src={"assets/img/brand_01.svg"}
+            alt="Brand 1"
+            className=" h-[80px] w-[90px]"
+          />
+        </div>
+        <div className="bg-white  py-10 rounded-[10px] mx-4 border px-6 ">
+          <img
+            src={"assets/img/brand_02.svg"}
+            alt="Brand 2"
+            className=" h-[80px] w-[90px]"
+          />
+        </div>
+        <div className="bg-white py-10 rounded-[10px] px-6">
+          <img
+            src={"assets/img/brand_03.svg"}
+            alt="Brand 3"
+            className=" h-[80px] w-[90px]"
+          />
+        </div>
+        <div className="bg-white py-10 px-6 rounded-[10px] ">
+          <img
+            src={"assets/img/brand_04.svg"}
+            alt="Brand 4"
+            className=" h-[80px] w-[90px]"
+          />
+        </div>
+        <div className="bg-white py-10 px-6 rounded-[10px]">
+          <img
+            src={"assets/img/brand_05.svg"}
+            alt="Brand 5"
+            className=" h-[80px] w-[90px]"
+          />
+        </div>
+        <div className="bg-white py-10 rounded-[10px] px-6">
+          <img
+            src={"assets/img/brand_06.svg"}
+            alt="Brand 6"
+            className=" h-[80px] w-[90px]"
+          />
+        </div>
+        <div className="bg-white py-10 rounded-[10px] px-6">
+          <img
+            src={"assets/img/brand_07.svg"}
+            alt="Brand 6"
+            className=" h-[80px] w-[90px]"
+          />
+        </div>
+
+        <div className="bg-white py-10 rounded-[10px] px-6">
+          <img
+            src={"assets/img/brand_08.svg"}
+            alt="Brand 6"
+            className=" h-[80px] w-[90px]"
+          />
+        </div>
+
+
+    </div>
+        </div>
+    
+    </>
+  );
+};
+
+export default Premium;
+// 
